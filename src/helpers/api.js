5780151cc0db9ac7c0c9394ee4365239729a9d1a
andrienag <107226588+andrienag@users.noreply.github.com>
2022-06-20 23:55:16 -0400

    modificaciones

---
+++ src/helpers/api.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+
+const searchCategory = async () => {
+  const instance = axios.create({
+    baseURL: "https://opentdb.com/api_category.php",
+    timeout: 1000,
+  });
+  try {
+    const { data } = await instance.get();
+    const { trivia_categories } = data
+    return {
+      done: true,
+      trivia_categories
+    };
+  } catch (err) {
+    return {
+      done: false,
+      err,
+    };
+  }
+};
+
+const questions = async (categoryID = 10) => {
+  const instance = axios.create({
+    baseURL: "https://opentdb.com/api.php",
+    timeout: 1000,
+    params: {
+      amount:50,
+      category: categoryID
+    },
+  });
+  try {
+    const { data } = await instance.get();
+    const { results } = data
+    return {
+      done: true,
+      results
+    };
+  } catch (err) {
+    return {
+      done: false,
+      err,
+    };
+  }
+};
+
+module.exports = {
+  searchCategory,
+  questions,
+};
